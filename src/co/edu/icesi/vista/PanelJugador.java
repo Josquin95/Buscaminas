fca126b8ced4f48e947ffd0cfc9de5d019d00077
Josquin <josquin@outlook.com>
2017-05-11 23:16:21 -0500

    Actualizacion del paquete vista

--- src/co/edu/icesi/vista/PanelJugador.java
+++ src/co/edu/icesi/vista/PanelJugador.java
@@ -5,46 +5,67 @@ import java.awt.*;
 
 public class PanelJugador extends InterfazBuscaMinas {
 
+    /**
+     * Ancho de la pantalla
+     */
     public final static int ANCHO = 800;
+
+    /**
+     * Largo de la pantalla
+     */
     public final static int LARGO = 600;
 
+    /**
+     * Nombre de la aplicacion
+     */
+    public final static String NOMBRE_APP = "Buscaminas";
+
+    /**
+     * Panel que contiene la matriz de botones
+     */
     private PanelTablero panelTablero;
+
+
     private JPanel panelSuperior;
     private JPanel panelDerecho;
     private JPanel panelInferior;
 
-	public PanelJugador() {
+    /**
+     * Constructor panel que se encarga de construir la interfaz de jugador
+     */
+    public PanelJugador() {
+        setTitle(NOMBRE_APP);
+        setSize(ANCHO, LARGO);
+        setResizable(false); // No se pemite la modificacion del tamaño
+        setLocationRelativeTo(null); // Su posicion siempre sera en el centro
+        setLayout(new BorderLayout());
 
-	    setTitle("Buscaminas");
-		setSize(ANCHO,LARGO);
-		setResizable(true);
-		setLocationRelativeTo(null);
-		setLayout(new BorderLayout());
+        construirPaneles();
+    }
 
-		panelSuperior = new JPanel();
-		panelSuperior.add(new JLabel("Panel Superior"));
-		add(panelSuperior, BorderLayout.NORTH);
+    private void construirPaneles(){
+        panelSuperior = new JPanel();
+        panelSuperior.add(new JLabel("Panel Superior"));
+        add(panelSuperior, BorderLayout.NORTH);
 
-		panelTablero = new PanelTablero(8); // TODO cambiar luego
-        add(panelTablero,BorderLayout.CENTER);
+        panelTablero = new PanelTablero(8); // TODO cambiar luego
+        add(panelTablero, BorderLayout.CENTER);
 
         panelDerecho = new JPanel();
         panelDerecho.add(new JLabel("Panel Derecho"));
-        add(panelDerecho,BorderLayout.EAST);
+        add(panelDerecho, BorderLayout.EAST);
 
         panelInferior = new JPanel();
         add(panelInferior, BorderLayout.SOUTH);
+    }
 
+    @Override
+    public void update() {
 
-	}
+    }
 
-	@Override
-	public void update() {
-
-	}
-
-	public static void main(String args[]){
-		PanelJugador pnlJugador = new PanelJugador();
-		pnlJugador.setVisible(true);
-	}
+    public static void main(String args[]) {
+        PanelJugador pnlJugador = new PanelJugador();
+        pnlJugador.setVisible(true);
+    }
 }
