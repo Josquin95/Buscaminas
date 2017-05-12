de0005181e008a2daa5d8fdce7157dc3f3b3dc08
Josquin <josquin@outlook.com>
2017-05-12 10:03:41 -0500

    Actualizacion del paquete vista

--- src/co/edu/icesi/modelo/CeldaMina.java
+++
@@ -1,8 +0,0 @@
-package co.edu.icesi.modelo;
-
-public class CeldaMina extends EstadoCelda {
-
-	public CeldaMina() {
-	}
-
-}
--- src/co/edu/icesi/modelo/CeldaVacia.java
+++
@@ -1,9 +0,0 @@
-package co.edu.icesi.modelo;
-
-public class CeldaVacia extends EstadoCelda {
-
-	public CeldaVacia() {
-		// TODO - implement CeldaVacia.CeldaVacia
-	}
-
-}
