0cc6c4cb00676aaa38333e3212e755fbf8bea062
Josquin <josquin@outlook.com>
2017-05-13 12:07:14 -0500

    Eliminacion de clases ineccesarias

--- src/main/java/co/edu/icesi/modelo/CeldaAmarilla.java
+++
@@ -1,7 +0,0 @@
-package co.edu.icesi.modelo;
-
-public class CeldaAmarilla extends AccesorioCelda {
-
-    public static final String AMARILLO = "Amarillo";
-
-}
--- src/main/java/co/edu/icesi/modelo/CeldaRoja.java
+++
@@ -1,4 +0,0 @@
-package co.edu.icesi.modelo;
-
-public class CeldaRoja extends AccesorioCelda {
-}
