db16dafceb1627fa16a26607f4e91f96febd2669
Carlos <catorres3007@gmail.com>
2017-05-18 15:20:16 -0500

    Clase para los eventos de la decoración

---
+++ src/main/java/co/edu/icesi/vista/MigestorDecorador.java
@@ -0,0 +1,17 @@
+package co.edu.icesi.vista;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by 1143929085 on 18/05/2017.
+ */
+public class MigestorDecorador implements ActionListener {
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
