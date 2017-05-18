2812ea88856aed65465e0e53b1830d0dbf66f306
Carlos <catorres3007@gmail.com>
2017-05-18 11:39:58 -0500

    Creo esta clase para que cada boton tenga un atributo con el valor de la posición.

--- src/main/java/co/edu/icesi/vista/BotonCuadricula.java
+++ src/main/java/co/edu/icesi/vista/BotonCuadricula.java
@@ -4,4 +4,19 @@ package co.edu.icesi.vista;
  * Created by 1143929085 on 18/05/2017.
  */
 public class BotonCuadricula {
+
+    public int valor;
+
+    public BotonCuadricula(int celda){
+        this.valor=celda;
+    }
+
+    public void setValor(int celda) {
+        this.valor = celda;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
 }
