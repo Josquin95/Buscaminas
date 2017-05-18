ad2f76efe8b38b1b4be9e3ebccb1d0f1017da321
Carlos <catorres3007@gmail.com>
2017-05-18 16:43:54 -0500

    Look and feels

--- src/main/java/co/edu/icesi/vista/MigestorDecorador.java
+++ src/main/java/co/edu/icesi/vista/MigestorDecorador.java
@@ -2,6 +2,8 @@ package co.edu.icesi.vista;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.UIManager;
+import javax.swing.*;
 
 /**
  * Created by CarlosTorres on 18/05/2017.
@@ -14,8 +16,26 @@ public class MigestorDecorador implements ActionListener {
  }
     @Override
     public void actionPerformed(ActionEvent e) {
-    if(e.getSource()==interfazJugador.getItemPrimero()){
+        if (e.getSource() == interfazJugador.getItemPrimero()) {
+            try {
+                UIManager.setLookAndFeel("com.birosoft.liquid.LiquidLookAndFeel");
+            } catch (Exception u) {
+                u.printStackTrace();
+            }
+            SwingUtilities.updateComponentTreeUI(interfazJugador);
+            interfazJugador.validate();
+        }
 
-    }
+
+        if (e.getSource() == interfazJugador.getItemSegundo()) {
+            try {
+                UIManager.setLookAndFeel("com.jtattoo.plaf.acryl.AcrylLookAndFeel");
+            } catch (Exception u) {
+                u.printStackTrace();
+            }
+
+            SwingUtilities.updateComponentTreeUI(interfazJugador);
+            interfazJugador.validate();
+        }
     }
 }
